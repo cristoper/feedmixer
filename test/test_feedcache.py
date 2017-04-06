7d8189df89e2d3c749ae91b1e5e6d5b18cf60da6
A. Cynic <chris@mretc.net>
2017-04-06 15:22:49 -0600

    Clean up tests

--- test/test_feedcache.py
+++ test/test_feedcache.py
@@ -155,50 +155,39 @@ class TestGet(unittest.TestCase):
         """If the database file does not exist, get() should return
         None."""
         mock_os_path_exists.return_value = False
-        fc = FeedCache(db_path='dummy', shelf_t=self.mock_shelf).get('fake_url')
-        self.mock_shelf.assert_not_called()
+        mock_shelf = MagicMock(spec=RWShelf)
+        fc = FeedCache(db_path='dummy', shelf_t=mock_shelf).get('fake_url')
+        mock_shelf.assert_not_called()
         self.assertEqual(fc, None)
 
     @patch('os.path.exists')
     def test_exists(self, mock_os_path_exists):
         """Test that FeedCache returns existing feed from shelf."""
         mock_os_path_exists.return_value = True
-        mock_get = MagicMock(return_value="fake result")
-        self.mock_shelf.return_value.__enter__.return_value.get = mock_get
-        fc = FeedCache(db_path='dummy', shelf_t=self.mock_shelf).get('fake_url')
-        self.mock_shelf.assert_called_with('dummy', 'r')
-        mock_get.assert_called_with('fake_url')
+        mock_shelf = mock_locked_shelf(return_value="fake result")
+        fc = FeedCache(db_path='dummy', shelf_t=mock_shelf).get('fake_url')
+
+        mock_shelf.assert_called_with('dummy', 'r')
+        mock_shelf.return_value.__enter__.return_value.get.assert_called_with('fake_url')
         self.assertEqual(fc, "fake result")
 
     @patch('os.path.exists')
     def test_not_exists(self, mock_os_path_exists):
-        """Test that FeedCache resturns None for key that doesn't exist in
+        """Test that FeedCache returns None for key that doesn't exist in
         shelf."""
         mock_os_path_exists.return_value = True
-        mock_get = MagicMock(return_value=None)
-        self.mock_shelf.return_value.__enter__.return_value.get = mock_get
-        fc = FeedCache(db_path='dummy', shelf_t=self.mock_shelf).get('fake_url')
-        self.mock_shelf.assert_called_with('dummy', 'r')
-        mock_get.assert_called_with('fake_url')
+        mock_shelf = mock_locked_shelf()
+        fc = FeedCache(db_path='dummy', shelf_t=mock_shelf).get('fake_url')
+
+        mock_shelf.assert_called_with('dummy', 'r')
+        mock_shelf.return_value.__enter__.return_value.get.assert_called_with('fake_url')
         self.assertEqual(fc, None)
 
 
 class TestUpdate(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        with open(ATOM_PATH, 'r') as f:
-            cls.test_atom = ''.join(f.readlines())
-            cls.test_atom_parsed = feedparser.parse(cls.test_atom)
-        with open(RSS_PATH, 'r') as f:
-            cls.test_rss = ''.join(f.readlines())
-            cls.test_rss_parsed = feedparser.parse(cls.test_rss)
-
-class TestFetch(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        with open(ATOM_PATH, 'r') as f:
-            cls.test_atom = ''.join(f.readlines())
-            cls.test_atom_parsed = feedparser.parse(cls.test_atom)
-        with open(RSS_PATH, 'r') as f:
-            cls.test_rss = ''.join(f.readlines())
-            cls.test_rss_parsed = feedparser.parse(cls.test_rss)
+    def test_setitem(self):
+        mock_shelf = mock_locked_shelf()
+        fc = FeedCache(db_path='dummy', shelf_t=mock_shelf)
+        fc.update('fake_url', 'fake_feed')
+        mock_shelf.assert_called_with('dummy', 'c')
+        mock_shelf.return_value.__enter__.return_value.__setitem__.assert_called_with('fake_url', 'fake_feed')
