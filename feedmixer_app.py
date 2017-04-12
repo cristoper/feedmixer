e99981fe2e469182be0d1a8d835bd9ef3a3cf3e3
A. Cynic <chris@mretc.net>
2017-04-11 19:25:15 -0600

    Parse 'n' query string and add some type annotations

--- feedmixer_app.py
+++ feedmixer_app.py
@@ -1,31 +1,40 @@
-from feedmixer import *
+from feedmixer import FeedMixer
 import falcon
+from typing import NamedTuple, List
 
-def get_feeds(req):
+ParsedQS = NamedTuple('ParsedQS', [('f', List[str]),
+                                   ('n', int)])
+
+
+def parse_qs(req: falcon.Request) -> ParsedQS:
     qs = falcon.uri.parse_query_string(req.query_string)
     feeds = qs.get('f', [])
+    n = qs.get('n', -1)
     if not isinstance(feeds, list): feeds = [feeds]
-    return feeds
+    return ParsedQS(feeds, int(n))
 
 
 class JSONFeed(object):
-    def on_get(self, req, resp):
-        feeds = get_feeds(req)
-        fm = FeedMixer(feeds=feeds)
+    def on_get(self, req: falcon.Request, resp: falcon.Response) -> None:
+        feeds, n = parse_qs(req)
+        fm = FeedMixer(feeds=feeds, num_keep=n)
         resp.body = fm.json_feed()
         resp.status = falcon.HTTP_200
 
+
 class AtomFeed(object):
-    def on_get(self, req, resp):
-        feeds = get_feeds(req)
-        fm = FeedMixer(feeds=feeds)
+    def on_get(self, req: falcon.Request, resp: falcon.Response) -> None:
+        feeds, n = parse_qs(req)
+        fm = FeedMixer(feeds=feeds, num_keep=n)
         resp.body = fm.atom_feed()
         resp.content_type = "application/atom"
         resp.status = falcon.HTTP_200
 
+
 class RSSFeed(object):
-    def on_get(self, req, resp):
-        fm = FeedMixer(feeds=feeds)
+    def on_get(self, req: falcon.Request, resp: falcon.Response) -> None:
+        feeds, n = parse_qs(req)
+        fm = FeedMixer(feeds=feeds, num_keep=n)
         resp.body = fm.rss_feed()
         resp.content_type = "application/rss"
         resp.status = falcon.HTTP_200
