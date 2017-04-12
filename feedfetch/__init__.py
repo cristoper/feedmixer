ed9a5a8d2766113678cb072bd2fd47660fd86e2c
A. Cynic <chris@mretc.net>
2017-04-12 11:58:08 -0600

    Split feedcache to its own package
    And rename it to feedfetch. This is in preparation to move it to its
    own repository.

---
+++ feedfetch/__init__.py
@@ -0,0 +1,1 @@
+from .feedfetch import FeedCache
--- feedfetch/feedfetch.py
+++ feedfetch/feedfetch.py
@@ -11,7 +11,7 @@ import datetime
 from http.client import NOT_MODIFIED
 import re
 import logging
-from locked_shelf import MutexShelf, RWShelf
+from feedfetch.locked_shelf import MutexShelf, RWShelf
 from typing import Union, Type, Callable, Optional
 
 locked_shelf_t = Union[Type[RWShelf], Type[MutexShelf]]
