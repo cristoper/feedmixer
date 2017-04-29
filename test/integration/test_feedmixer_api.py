4dd080a4794476072f7ab26791ccd45d9c077dc4
A. Cynic <chris@mretc.net>
2017-04-29 01:06:31 -0600

    Integration test to excercise prefer_summary

--- test/integration/test_feedmixer_api.py
+++ test/integration/test_feedmixer_api.py
@@ -8,10 +8,12 @@ import os
 TESTDB = 'fm_test_cache'
 
 
-def build_qs(feeds=[], n=-1):
+def build_qs(feeds=[], n=-1, full=False):
     feeds = ["f={}".format(f) for f in feeds]
     qs = '&'.join(feeds)
     qs += "&n={}".format(n)
+    if full:
+        qs += '&full=y'
     return qs
 
 
@@ -64,3 +66,27 @@ class TestAtom(FMTestCase):
         self.assertFalse(atom.bozo)
         self.assertTrue(len(atom.entries) == 4)
         self.assertIsNone(errors)
+
+    def test_prefer_summary(self):
+        qs = build_qs(feeds=['http://americancynic.net/atom.xml'], n=1)
+        qs_full = build_qs(feeds=['http://americancynic.net/atom.xml'], n=1,
+                           full=True)
+        result, _ = self.get_results_errors(path='/atom', qs=qs)
+        result_full, _ = self.get_results_errors(path='/atom', qs=qs_full)
+        atom = feedparser.parse(result.text)
+        atom_full = feedparser.parse(result_full.text)
+
+        e = atom.entries[0]
+
+        # Find entry in e_full with same unique_id to ensure we are comparing
+        # the same entry from each fetch.
+        eid = e.get('id')
+        for entry in atom_full.entries:
+            if entry.get('id') == eid:
+                e_full = entry
+                break
+
+        e_desc = e.get('description')
+        e_full_desc = e_full.get('description')
+        self.assertEqual(eid, e_full.get('id'))
+        self.assertTrue(len(e_desc) < len(e_full_desc))
